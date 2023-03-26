6230e1937196dd73620c32c5aa707d16a79f15ac
Abhishek <abhikharpal@gmail.com>
2023-03-26 15:41:26 +0530

    chore: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+/**
+ * @return {JSX.Element}
+ * @description
+ * This is the main component of the application.
+ * It is the entry point of the application.
+ */
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
